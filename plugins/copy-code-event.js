01b9232682b04aa00e59d828c5e639ace5afede9
heart <7362469@qq.com>
2023-07-15 00:05:13 +0800

    fix: fix build error

--- plugins/copy-code-event.js
+++ plugins/copy-code-event.js
@@ -1,7 +1,18 @@
-import { copy } from '@cc-heart/utils-client'
+function copy(text) {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(text)
+    return
+  }
+  console.warn('clipboard is not exist of navigator')
+}
 if (typeof window === 'object' && window !== null) {
   Reflect.set(window, '__copyCode', function (target) {
-    const code = target.getAttribute('value')
-    copy(code)
+    let code = target.getAttribute('value')
+    if (code) {
+      code = decodeURIComponent(atob(code))
+      console.log(code)
+
+      copy(code)
+    }
   })
 }
--- plugins/markdown-it-code-copy.cjs
+++ plugins/markdown-it-code-copy.cjs
@@ -4,13 +4,16 @@ module.exports = function (md) {
     const token = tokens[idx]
     const code = token.content
     const lang = token.info ? token.info.trim() : ''
-    const escapeHtmlCode = md.utils.escapeHtml(code)
-    const highlighted = options.highlight(code, lang) || escapeHtmlCode
+    const highlighted =
+      options.highlight(code, lang) || md.utils.escapeHtml(code)
+    const base64Code = Buffer.from(encodeURIComponent(code), 'utf-8').toString(
+      'base64'
+    )
 
     // 生成复制按钮
     const btnHtml =
       '<button class="copy-btn" value="' +
-      escapeHtmlCode +
+      base64Code +
       '" onclick="__copyCode(this)">' +
       '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M216 34H88a6 6 0 0 0-6 6v42H40a6 6 0 0 0-6 6v128a6 6 0 0 0 6 6h128a6 6 0 0 0 6-6v-42h42a6 6 0 0 0 6-6V40a6 6 0 0 0-6-6Zm-54 176H46V94h116Zm48-48h-36V88a6 6 0 0 0-6-6H94V46h116Z"/></svg>' +
       '</button>'
