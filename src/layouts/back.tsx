5b3765ebf7be704b472aafce458a116006b4bef3
heart <7362469@qq.com>
2023-08-27 00:54:53 +0800

    perf: remove format empty string

--- src/layouts/back.tsx
+++ src/layouts/back.tsx
@@ -6,15 +6,13 @@ export default defineComponent({
   setup() {
     const router = useRouter()
     const route = useRoute()
-    const goBack = () => {
-      router.go(-1)
-    }
+    const goBack = () => router.go(-1)
     if (route.path === '/') return () => null
     return () => (
       <div class="cc-back m-y-5">
         <span class="m-r-2">&gt;</span>
         <span class="whitespace-pre" onClick={goBack}>
-          cd ..{' '}
+          cd ..
         </span>
       </div>
     )
