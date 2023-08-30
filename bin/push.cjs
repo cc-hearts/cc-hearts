315958e429c7d42a300a4c2c9a5904656b29e733
heart <7362469@qq.com>
2023-08-31 02:25:12 +0800

    fix: optimize push scripts

--- bin/push.cjs
+++ bin/push.cjs
@@ -1,10 +1,28 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process')
 
-const git = spawn(
-  `export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890; git push`
-)
+const proxyCommand =
+  'export https_proxy=http://127.0.0.1:7890 && export http_proxy=http://127.0.0.1:7890 && export all_proxy=socks5://127.0.0.1:7890'
+const gitPushCommand = 'git push'
 
-git.stdout.on('data', (data) => {
-  console.log(data)
+const proxyProcess = spawn('bash', ['-c', proxyCommand])
+
+proxyProcess.on('exit', (code) => {
+  if (code === 0) {
+    const gitPushProcess = spawn('bash', ['-c', gitPushCommand])
+
+    gitPushProcess.stdout.on('data', (data) => {
+      console.log(data.toString())
+    })
+
+    gitPushProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+    })
+
+    gitPushProcess.on('close', (code) => {
+      console.log(`Git push process exited with code ${code}`)
+    })
+  } else {
+    console.error('Failed to set proxy environment variables')
+  }
 })
