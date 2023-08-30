351b8446d364be289e7c8ea7c4e600c3f6149933
heart <7362469@qq.com>
2023-08-31 02:06:06 +0800

    fix: fix scripts name suffix

--- bin/push.cjs
+++ bin/push.cjs
@@ -2,7 +2,7 @@
 const { spawn } = require('child_process')
 
 const git = spawn(
-  `export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 && git push`
+  `export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890; git push`
 )
 
 git.stdout.on('data', (data) => {
