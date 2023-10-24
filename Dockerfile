f0888e938f490b13d97cb54a289c033c8384c4f8
heart <7362469@qq.com>
2023-10-24 17:05:01 +0800

    chore: add nginx https

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,7 @@ LABEL MAINTAINER="heart<7362469@qq.com>"
 
 COPY dist/ /opt/cc-hearts/
 
+COPY ssl.pem /etc/nginx/ssl/
+COPY ssl.key /etc/nginx/ssl/
+
 COPY default.conf /etc/nginx/conf.d/default.conf
--- bin/build.sh
+++ bin/build.sh
@@ -4,4 +4,4 @@ docker rm -f cc-hearts
 docker rmi -f cc-hearts:1.0.0
 docker build -t cc-hearts:1.0.0 .
 
-docker run -p 80:80 -v /opt/cc-hearts/dist/:/opt/cc-hearts/ --name cc-hearts -d cc-hearts:1.0.0
+docker run -p 80:80 -p 443:443 -v /opt/cc-hearts/dist/:/opt/cc-hearts/ --name cc-hearts -d cc-hearts:1.0.0
