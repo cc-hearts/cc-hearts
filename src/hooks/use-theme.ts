73451f5ac09db4ad8063da5c054b025f58589d27
heart <7362469@qq.com>
2023-11-02 11:06:27 +0800

    fix: wrapper ssg

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -1,17 +1,20 @@
 import { ref } from 'vue'
 import { useOnce } from './use-once'
 import { THEME } from '@/configs/constants'
+import { isBrowser } from '@/utils/valid'
 
 export const useTheme = useOnce(() => {
-  const classList = document.documentElement.classList
-  const themeRef = ref(
-    classList.contains(THEME.DARK) ? THEME.DARK : THEME.LIGHT
-  )
+  const themeRef = ref(THEME.LIGHT)
+
   const setTheme = (theme: THEME) => {
     themeRef.value = theme
-    document.documentElement.classList.remove(THEME.DARK, THEME.LIGHT)
-    document.documentElement.classList.add(theme)
+
+    if (isBrowser()) {
+      document.documentElement.classList.remove(THEME.DARK, THEME.LIGHT)
+      document.documentElement.classList.add(theme)
+    }
   }
+
   const toggleTheme = () => {
     const hasExistDarkCls = document.documentElement.classList.contains(
       THEME.DARK
@@ -19,6 +22,12 @@ export const useTheme = useOnce(() => {
     const appendTheme = hasExistDarkCls ? THEME.LIGHT : THEME.DARK
     setTheme(appendTheme)
   }
+
+  if (isBrowser()) {
+    const classList = document.documentElement.classList
+    setTheme(classList.contains(THEME.DARK) ? THEME.DARK : THEME.LIGHT)
+  }
+
   return [themeRef, setTheme, toggleTheme] as const
 })
 
