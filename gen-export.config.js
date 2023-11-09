59fb1ff37467e88b1e35833ac9064f6e36f48a6b
heart <7362469@qq.com>
2023-11-09 23:32:00 +0800

    chore: update config

---
+++ gen-export.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from '@cc-heart/gen-index-export'
+export default defineConfig({
+  dirs: [
+    { path: 'src/hooks', output: 'src/hooks/index.ts' },
+    { path: 'src/icons', output: 'src/icons/index.ts' },
+  ],
+})
